d0536da8fc105be71e3a6d2b35dda80d5e3cfdc5
Somsubhra <somsubhramisra19@gmail.com>
2026-09-10 17:41:35 +0530

    Bind Express server to 0.0.0.0 to accept external connections

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
-app.listen(env.port, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+// app.listen(env.port, () => {
+//     console.log(`Server is running on port ${process.env.PORT || 3000}`);
+// });
+
+app.listen(env.port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${env.port}`);
 });
