4a81531d85cccf9ea57b4ddddde2d7cf1c1d131f
Somsubhra <somsubhramisra19@gmail.com>
2026-09-10 17:49:12 +0530

    fix: update User model import to match file system casing

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 const ApiError = require("../utils/ApiError");
 
 const registerUser = async ({ name, email, password, role }) => {
